1d31d17cac6ecec218d54a638d3549719619154b
DONELSINHA01 <100290944+DONELSINHA01@users.noreply.github.com>
2023-04-06 15:48:16 +0530

    Add files via upload

---
+++ Assignment 17/src/App.js
@@ -0,0 +1,15 @@
+import A from './ContextAPI-1/A';
+import './App.css';
+
+
+function App() {
+
+    return(
+      <div className='App'>
+      <A/>
+      </div>
+    );
+    
+}
+
+export default App;
